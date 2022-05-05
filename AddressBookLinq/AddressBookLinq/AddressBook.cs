b0e491998d2e3ab1eb396db074c3db2d96f5ab77
Remant <remantmahato9798@gmail.com>
2022-05-05 07:42:25 +0530

    Added : UC3-InsertContactAddressBook

--- AddressBookLinq/AddressBookLinq/AddressBook.cs
+++ AddressBookLinq/AddressBookLinq/AddressBook.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Data;
+using AddressBookLinq;
 
-namespace AddressBookLinq
+namespace AddressBooklinq
 {
     class AddressBook
     {
-        public void CreateAddressBookDataTable()
+        DataTable dataTable = new DataTable();
+
+        public AddressBook()
         {
-            DataTable dataTable = new DataTable();
             dataTable.Columns.Add("FirstName", typeof(string));
             dataTable.Columns.Add("LastName", typeof(string));
             dataTable.Columns.Add("Address", typeof(string));
@@ -18,6 +20,31 @@ namespace AddressBookLinq
             dataTable.Columns.Add("Zip", typeof(int));
             dataTable.Columns.Add("PhoneNumber", typeof(long));
             dataTable.Columns.Add("Email", typeof(string));
+        }
+        public void AddContact()
+        {
+            dataTable.Rows.Add("Remant", "Mahato", "Jorapokhar", "Dhanbad", "Jharkhand", 828112, 8271630771, "remantmahato9798@gmail.com");
+            dataTable.Rows.Add("Hemant", "Mahato", "Jorapokhar", "Dhanbad", "Jharkhand", 828112, 7979367171, "hemant402@gmail.com");
+            dataTable.Rows.Add("Aman", "Singh", "Newtown", "Kolkata", "WestBengal", 7003213, 9572846335, "aman147@gmail.com");
+            dataTable.Rows.Add("Shubham", "Singh", "Sector-2", "Bokaro", "Jharkhand", 831005, 8271453692, "shubham145@gmail.com");
+            dataTable.Rows.Add("Ajit", "Kumar", "Ashoknagar", "Patna", "Bihar", 865112, 9571593571, "ajit957@gmail.com");
+            Console.WriteLine("Contact is Added in Address Book ");
+
+        }
+        public void DisplayContacts()
+        {
+            foreach (var contact in dataTable.AsEnumerable())
+            {
+                Console.WriteLine("First Name:" + contact.Field<string>("FirstName"));
+                Console.WriteLine("LastName:-" + contact.Field<string>("LastName"));
+                Console.WriteLine("Address:-" + contact.Field<string>("Address"));
+                Console.WriteLine("City:-" + contact.Field<string>("City"));
+                Console.WriteLine("State:-" + contact.Field<string>("State"));
+                Console.WriteLine("ZipCode:-" + contact.Field<int>("Zip"));
+                Console.WriteLine("PhoneNumber:-" + contact.Field<long>("PhoneNumber"));
+                Console.WriteLine("Email:-" + contact.Field<string>("Email"));
+                Console.WriteLine();
+            }
         }
     }
 }
--- AddressBookLinq/AddressBookLinq/Program.cs
+++ AddressBookLinq/AddressBookLinq/Program.cs
@@ -1,14 +1,40 @@
-﻿using System;
+﻿using AddressBooklinq;
+
+using System;
 using System.Data;
 
 namespace AddressBookLinq
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Address Book Using LINQ");
-            AddressBook addressbook = new AddressBook();
+            AddressBook address = new AddressBook();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1: For Add the Contact");
+                Console.WriteLine("2: For Display Contact");
+                Console.WriteLine("0: For Exist");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        address.AddContact();
+                        break;
+                    case 2:
+                        address.DisplayContacts();
+                        break;
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input Please Choose Correct Option");
+                        break;
+                }
+            }
+            while (option != 0);
         }
     }
 }
